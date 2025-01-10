9530cd7889dbec13f76cdfb943fff2a28a896917
Franquelie Masanque <137916686+franquelie@users.noreply.github.com>
2025-01-10 22:32:10 +0800

    in progress

--- app/src/main/java/com/example/gameuntitled/MainActivity.java
+++ app/src/main/java/com/example/gameuntitled/MainActivity.java
@@ -1,23 +1,28 @@
 package com.example.gameuntitled;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context; 
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+/**
+ * MainActivity is the entry point to our application.
+ */
 
 public class MainActivity extends AppCompatActivity {
-
-    private static Context gameContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        gameContext = this;
-        setContentView(new GamePanel(this));
-    }
 
-    public static Context getGameContext() {
-        return gameContext;
+        // Set window to fullscreen (will hide status bar)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Set content view to game, so that objects in the Game class can be rendered to the screen
+        setContentView(new Game(this));
     }
 }
