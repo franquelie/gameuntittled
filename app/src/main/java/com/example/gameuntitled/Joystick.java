b266c353d13d13df1085214dc2a881f801a68cdc
Franquelie Masanque <137916686+franquelie@users.noreply.github.com>
2025-01-19 14:07:06 +0800

    in progress

---
+++ app/src/main/java/com/example/gameuntitled/Joystick.java
@@ -0,0 +1,62 @@
+package com.example.gameuntitled;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class Joystick {
+
+    private int outerCircleRadius;
+    private int innerCircleRadius;
+    private int outerCircleCenterPositionX;
+    private int outerCircleCenterPositionY;
+    private int innerCircleCenterPositionX;
+    private int innerCircleCenterPositionY;
+    private Paint innerCirclePaint;
+    private Paint outerCirclePaint;
+
+
+    public Joystick(int centerPositionX, int centerPositionY, int outerCircleRadius, int innerCircleRadius) {
+
+        // Outer and inner circle make up the joystick
+        outerCircleCenterPositionX = centerPositionX;
+        outerCircleCenterPositionY = centerPositionY;
+        innerCircleCenterPositionX = centerPositionX;
+        innerCircleCenterPositionY = centerPositionY;
+
+        // Radii of circles
+        this.outerCircleRadius = outerCircleRadius;
+        this.innerCircleRadius = innerCircleRadius;
+
+        // Paint of circles
+        outerCirclePaint = new Paint();
+        outerCirclePaint.setColor(Color.GRAY);
+        outerCirclePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        innerCirclePaint = new Paint();
+        innerCirclePaint.setColor(Color.BLUE);
+        innerCirclePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        
+
+    }
+    public void draw(Canvas canvas) {
+        // Draw outer circle
+        canvas.drawCircle(
+                outerCircleCenterPositionX,
+                outerCircleCenterPositionY,
+                outerCircleRadius,
+                outerCirclePaint
+        );
+
+        // Draw inner circle
+        canvas.drawCircle(
+                innerCircleCenterPositionX,
+                innerCircleCenterPositionY,
+                innerCircleRadius,
+                innerCirclePaint
+        );
+    }
+
+    public void update() {
+    }
+}
