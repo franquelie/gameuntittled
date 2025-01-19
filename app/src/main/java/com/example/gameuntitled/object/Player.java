e826a04d0450372420fcac3b40389da34868c482
Franquelie Masanque <137916686+franquelie@users.noreply.github.com>
2025-01-19 17:11:42 +0800

    in progress

---
+++ app/src/main/java/com/example/gameuntitled/object/Player.java
@@ -0,0 +1,40 @@
+package com.example.gameuntitled.object;
+
+import android.content.Context;
+
+import androidx.core.content.ContextCompat;
+
+import com.example.gameuntitled.GameLoop;
+import com.example.gameuntitled.Joystick;
+import com.example.gameuntitled.R;
+
+
+/**
+ * Player is the main character of the game, which the user can control with a touch joystick.
+ * The player class is an extension of a Circle, which is an extension of a GameObject
+ */
+public class Player extends Circle {
+    public static final double SPEED_PIXELS_PER_SECOND = 400.0;
+    public static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.MAX_UPS;
+    private final Joystick joystick;
+
+    public Player(Context context, Joystick joystick, double positionX, double positionY, double radius) {
+        super(context, ContextCompat.getColor(context, R.color.player), positionX, positionY, radius);
+        this.joystick = joystick;
+    }
+
+    public void update() {
+        // Update velocity based on actuator of joystick
+        velocityX = joystick.getActuatorX()*MAX_SPEED;
+        velocityY = joystick.getActuatorY()*MAX_SPEED;
+
+        // Update position
+        positionX += velocityX;
+        positionY += velocityY;
+    }
+
+    public void setPosition(double positionX, double positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+}
