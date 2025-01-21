635315fb966f7923bbc8475582e6906754d0ba25
Franquelie Masanque <137916686+franquelie@users.noreply.github.com>
2025-01-21 11:58:19 +0800

    in progress

--- app/src/main/java/com/example/gameuntitled/object/Enemy.java
+++ app/src/main/java/com/example/gameuntitled/object/Enemy.java
@@ -13,11 +13,41 @@ import com.example.gameuntitled.R;
 public class Enemy extends Circle {
     private static final double SPEED_PIXELS_PER_SECOND = Player.SPEED_PIXELS_PER_SECOND*0.6;
     private static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.MAX_UPS;
+    private static double SPAWNS_PER_MINUTE = 20;
+    private static double SPAWNS_PER_SECOND = SPAWNS_PER_MINUTE / 60;
+    private static double UPDATES_PER_SPAWN = GameLoop.MAX_UPS/SPAWNS_PER_SECOND;
+    private static double updateUntilNextSpawn = UPDATES_PER_SPAWN;
     private final Player player;
 
     public Enemy(Context context, Player player, double positionX, double positionY, double radius) {
         super(context, ContextCompat.getColor(context, R.color.enemy), positionX, positionY, radius);
         this.player = player;
+    }
+
+    public Enemy(Context context, Player player) {
+        super(
+            context,
+            ContextCompat.getColor(context, R.color.enemy),
+            Math.random() * 1000,
+            Math.random() * 1000,
+            30  
+        );
+        this.player = player;
+    }
+
+    /**
+     * readyToSpawn checks if a new enemy should spawn, according to the decided number of spawns
+     * per minute (see SPAWNS_PER_MINUTE at top)
+     * @return
+     */
+    public static boolean readyToSpawn() {
+        if (updateUntilNextSpawn < 0) {
+            updateUntilNextSpawn += UPDATES_PER_SPAWN;
+            return true;
+        } else {
+            updateUntilNextSpawn--;
+            return false;
+        }
     }
 
     @Override
