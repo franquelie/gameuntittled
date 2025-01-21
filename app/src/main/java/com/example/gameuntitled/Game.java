9ea9111e0958de56e5f4864e904d1e1ed9777831
Franquelie Masanque <137916686+franquelie@users.noreply.github.com>
2025-01-21 20:55:45 +0800

    in progress

--- app/src/main/java/com/example/gameuntitled/Game.java
+++ app/src/main/java/com/example/gameuntitled/Game.java
@@ -1,152 +1,53 @@
-package com.example.gameuntitled;
+package com.example.androidstudio2dgamedevelopment.gameobject;
 
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import androidx.core.content.ContextCompat;
 
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-
-import com.example.gameuntitled.object.Enemy;
-import com.example.gameuntitled.object.Player;
-import com.example.gameuntitled.object.Spell;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.example.androidstudio2dgamedevelopment.GameDisplay;
 
 /**
- * Game manages all objects in the game and is responsible for updating all states and render all
- * objects to the screen
+ * Circle is an abstract class which implements a draw method from GameObject for drawing the object
+ * as a circle.
  */
-class Game extends SurfaceView implements SurfaceHolder.Callback {
-    private final Joystick joystick;
-    private final Player player;
-    private GameLoop gameLoop;
-    private List<Enemy> enemyList = new ArrayList<Enemy>();
-    private List<Spell> spellList = new ArrayList<Spell>();
+public abstract class Circle extends GameObject {
+    protected double radius;
+    protected Paint paint;
 
-    public Game(Context context) {
-        super(context);
+    public Circle(Context context, int color, double positionX, double positionY, double radius) {
+        super(positionX, positionY);
+        this.radius = radius;
 
-        // Get surface holder and add callback
-        SurfaceHolder surfaceHolder = getHolder();
-        surfaceHolder.addCallback(this);
-
-        gameLoop = new GameLoop(this, surfaceHolder);
-
-        // Initialize game objects
-        joystick = new Joystick(275, 700, 70, 40);
-        player = new Player(getContext(), joystick, 2*500, 500, 30);
-
-        setFocusable(true);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-        // Handle touch event actions
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                if (joystick.getIsPressed()) {
-                    // Joystick was pressed before this event -> cast spell
-                    spellList.add(new Spell(getContext(), player));
-                } else if (joystick.isPressed((double) event.getX(), (double) event.getY())) {
-                    // Joystick is pressed in this event -> setIsPressed(true)
-                    joystick.setIsPressed(true);
-                } else {
-                    // Joystick was not pressed previously, and is not pressed in this event -> cast spell
-                    spellList.add(new Spell(getContext(), player));
-                }
-                return true;
-            case MotionEvent.ACTION_MOVE:
-                // Joystick was pressed previously and is now moved
-                if(joystick.getIsPressed()) {
-                    joystick.setActuator((double) event.getX(), (double) event.getY());
-                }
-                return true;
-            case MotionEvent.ACTION_UP:
-                // Joystick was let go off -> setIsPressed(false) and resetActuator
-                joystick.setIsPressed(false);
-                joystick.resetActuator();
-                return true;
-        }
-
-        return super.onTouchEvent(event);
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        gameLoop.startLoop();
-    }
-
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
-    }
-
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-        drawUPS(canvas);
-        drawFPS(canvas);
-
-        joystick.draw(canvas);
-        player.draw(canvas);
-
-        for (Enemy enemy : enemyList) {
-            enemy.draw(canvas);
-        }
-
-        for (Spell spell : spellList) {
-            spell.draw(canvas);
-        }
-
-    }
-
-    public void drawUPS(Canvas canvas) {
-        String averageUPS = Double.toString(gameLoop.getAverageUPS());
-        Paint paint = new Paint();
-        int color = ContextCompat.getColor(getContext(), R.color.magenta);
+        // Set colors of circle
+        paint = new Paint();
         paint.setColor(color);
-        paint.setTextSize(50);
-        canvas.drawText("UPS: " + averageUPS, 100, 150, paint); // Changed y to 150
     }
 
-    public void drawFPS(Canvas canvas) {
-        String averageFPS = Double.toString(gameLoop.getAverageFPS());
-        Paint paint = new Paint();
-        int color = ContextCompat.getColor(getContext(), R.color.magenta);
-        paint.setColor(color);
-        paint.setTextSize(50);
-        canvas.drawText("FPS: " + averageFPS, 100, 250, paint); // Changed y to 250
+    /**
+     * isColliding checks if two circle objects are colliding, based on their positions and radii.
+     * @param obj1
+     * @param obj2
+     * @return
+     */
+    public static boolean isColliding(Circle obj1, Circle obj2) {
+        double distance = getDistanceBetweenObjects(obj1, obj2);
+        double distanceToCollision = obj1.getRadius() + obj2.getRadius();
+        if (distance < distanceToCollision)
+            return true;
+        else
+            return false;
     }
 
-    public void update() {
-        // Update game state
-        joystick.update();
-        player.update();
+    public void draw(Canvas canvas, GameDisplay gameDisplay) {
+        canvas.drawCircle(
+                (float) gameDisplay.gameToDisplayCoordinatesX(positionX),
+                (float) gameDisplay.gameToDisplayCoordinatesY(positionY),
+                (float) radius,
+                paint
+        );
+    }
 
-        // Spawn enemy if it is time to spawn new enemies
-        if(Enemy.readyToSpawn()) {
-            enemyList.add(new Enemy(getContext(), player));
-        }
-
-        // Update states of each enemy
-        for (Enemy enemy : enemyList) {
-            enemy.update();
-        }
-
-        // Update states of each spell
-        for (Spell spell : spellList) {
-            spell.update();
-        }
+    public double getRadius() {
+        return radius;
     }
 }
