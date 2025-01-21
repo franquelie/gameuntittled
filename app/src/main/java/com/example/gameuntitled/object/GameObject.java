7bb49699d66c8128c76ec0407aea7e9e2675f095
Franquelie Masanque <137916686+franquelie@users.noreply.github.com>
2025-01-21 18:43:38 +0800

    in progress

--- app/src/main/java/com/example/gameuntitled/object/GameObject.java
+++ app/src/main/java/com/example/gameuntitled/object/GameObject.java
@@ -6,26 +6,35 @@ import android.graphics.Canvas;
  * GameObject is an abstract class which is the foundation of all world objects in the game.
  */
 public abstract class GameObject {
-    protected double positionX;
-    protected double positionY;
-    protected double velocityX;
-    protected double velocityY;
+    protected double positionX = 0;
+    protected double positionY = 0;
+    protected double velocityX = 0;
+    protected double velocityY = 0;
+    protected double directionX = 1;
+    protected double directionY = 0;
 
     public GameObject(double positionX, double positionY) {
         this.positionX = positionX;
         this.positionY = positionY;
     }
 
-    protected static double getDistanceBetweenObjects(GameObject obj1, GameObject obj2) {
-        return Math.sqrt(
-                Math.pow(obj1.getPositionX() - obj2.getPositionX(), 2) +
-                Math.pow(obj1.getPositionY() - obj2.getPositionY(), 2)
-        );
-    }
+    protected double getPositionX() { return positionX; }
+    protected double getPositionY() { return positionY; }
+
+    protected double getDirectionX() { return directionX; }
+    protected double getDirectionY() { return directionY; }
 
     public abstract void draw(Canvas canvas);
     public abstract void update();
 
-    protected double getPositionX() { return positionX; }
-    protected double getPositionY() { return positionY; }
+    protected static double getDistanceBetweenObjects(GameObject obj1, GameObject obj2) {
+        return Math.sqrt(
+                Math.pow(obj2.getPositionX() - obj1.getPositionX(), 2) +
+                Math.pow(obj2.getPositionY() - obj1.getPositionY(), 2)
+        );
+    }
+
+
+
+
 }
